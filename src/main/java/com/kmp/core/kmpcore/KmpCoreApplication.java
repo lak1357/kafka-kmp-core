8f293e7a5179e0803b72efc50aa82ca9825f0cae
Lakshitha Herath (543830003) <lakshitha.herath@moneta.cz>
2018-06-18 00:59:21 +0530

    init project

---
+++ src/main/java/com/kmp/core/kmpcore/KmpCoreApplication.java
@@ -0,0 +1,12 @@
+package com.kmp.core.kmpcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmpCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmpCoreApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kmp/core/kmpcore/KmpCoreApplicationTests.java
@@ -0,0 +1,39 @@
+package com.kmp.core.kmpcore;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kmp.core.kmpcore.consumer.Receiver;
+import com.kmp.core.kmpcore.producer.Sender;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KmpCoreApplicationTests {
+
+	private static String BOOT_TOPIC = "boot.t";
+
+	@Autowired
+	private Sender sender;
+
+	@Autowired
+	private Receiver receiver;
+
+	@ClassRule
+	public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, BOOT_TOPIC);
+
+	@Test
+	public void testReceive() throws Exception {
+		sender.send(BOOT_TOPIC, "Hello Booooooooooooooooooooooooooooooooooooooooooooot!");
+		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+	}
+}
